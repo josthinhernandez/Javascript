430175237b1b95951f7a89cd7f8b3dbccd9dd483
Josthin Hernandez <tehozolhernandezjosthin@gmail.com>
2021-07-05 12:48:44 -0500

    snapshot

---
+++ __test__/snapshot.test.js
@@ -0,0 +1,9 @@
+import { getCharacter } from "../snapshot";
+
+import rick from "../rick.json";
+
+describe('Es hora de las instantaneas', () => {
+    test("Snapshot", () => {
+        expect(getCharacter(rick)).toMatchSnapshot();
+    })
+})

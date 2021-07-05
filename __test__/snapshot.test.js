612e649e0b5f88018d3e5ca9b608ced9428f05fc
Josthin Hernandez <tehozolhernandezjosthin@gmail.com>
2021-07-05 12:59:48 -0500

    se agregaron test en snapshot

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,5 +5,23 @@ import rick from "../rick.json";
 describe('Es hora de las instantaneas', () => {
     test("Snapshot", () => {
         expect(getCharacter(rick)).toMatchSnapshot();
+    });
+
+    test("Siempre fallara la instantanea", () => {
+        const user = {
+            createAt: new Date(),
+            id: Math.floor(Math.random() * 20),
+        }
+        expect(user).toMatchSnapshot();
     })
+
+    test("Tenemos una excepcion dentro de codigo", () => {
+        const user = {
+            id: Math.floor(Math.random() * 20),
+            name: "Josthin Hernandez"
+        }
+        expect(user).toMatchSnapshot({
+            id: expect.any(Number)
+        })
+    });
 })

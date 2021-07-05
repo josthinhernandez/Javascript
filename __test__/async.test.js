6248d8842208535484944ca7de9fffa6be1f4bdd
Josthin Hernandez <tehozolhernandezjosthin@gmail.com>
2021-07-05 12:36:58 -0500

    sApi con error

--- __test__/async.test.js
+++ __test__/async.test.js
@@ -7,8 +7,19 @@ describe("Probar Async/Await", () => {
         await getDataFromApi(api).then(data => {
             expect(data.results.length).toBeGreaterThan(0);
         });
+
         await getDataFromApi(getRick).then(data => {
             expect(data.name).toEqual("Rick Sanchez");
         })
+    })
+    test("Realizando una peticion a una api con error", async() => {
+        const apiError = "http://httpstat.us/500";
+        const peticion = getDataFromApi(apiError);
+        await expect(peticion).rejects.toEqual(Error('Request failed with status code 500'));
+    })
+
+    test("Resuleve un hola", async() => {
+        await expect(Promise.resolve("Hola")).resolves.toBe("Hola");
+        await expect(Promise.reject("Error")).rejects.toBe("Error");
     })
 })

a753500cb97e80cb9c720a87f8abb36851286305
Josthin Hernandez <tehozolhernandezjosthin@gmail.com>
2021-07-04 20:02:43 -0500

    Creacion de test de array

---
+++ __test__/arrays.test.js
@@ -0,0 +1,14 @@
+import { arrayFruits, arrayColors } from "../arrays";
+
+describe("Comprobaremos que existe un elemento", () => {
+    test("¿Tiene una banana?", () => {
+        expect(arrayFruits()).toContain("banana");
+    })
+    test("No contiene un platano", () => {
+        expect(arrayFruits()).not.toContain("platano");
+    })
+    test("Comprobaremos el tamaño de un arreglo", () => {
+        expect(arrayFruits()).toHaveLength(6);
+    })
+
+})
